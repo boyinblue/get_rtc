7018b4e3163c7fb93d595159654f8d767a65bc03
boyinblue <50429025+boyinblue@users.noreply.github.com>
2022-11-11 14:35:20 +0900

    Update get_rtc.sh

--- get_rtc.sh
+++ get_rtc.sh
@@ -3,20 +3,32 @@
 #current_time=$(ssh parksejin@175.118.12.219 "date '+%Y-%m-%d %H:%M:%S'")
 
 ips=("15.26.228.11" "www.dhqhrtnwl.shop")
+skip_ping=false
 
 function get_rtc
 {
   ip="${1}"
 
-  echo "ping -c 1 $ip"
-  ping -c 1 "${ip}"
-
-  if [ "$?" == "0" ]; then
-    current_time=$(ssh parksejin@${ip} "date '+%Y-%m-%d %H:%M:%S'")
-    timedatectl set-time "$current_time"
-    echo "RTC setting completed"
-    exit 0
+  if [ $skip_ping == false ]; then
+    echo "ping -c 1 $ip"
+    ping -c 1 "${ip}"
+    if [ "$?" != "0" ]; then
+      return
+    fi
   fi
+
+  current_time=$(ssh parksejin@${ip} "date '+%Y-%m-%d %H:%M:%S'")
+  timedatectl set-time "$current_time"
+  echo "RTC setting completed"
+  exit 0
+}
+
+function print_usage
+{
+  echo "(Usage) ${0} [IP] [-no_ping]"
+  echo "(Example) ${0}"
+  echo "(Example) ${0} www.dhqhrtnwl.shop"
+  echo "(Example) ${0} www.dhqhrtnwl.shop -no_ping"
 }
 
 if [ "${1}" != "" ]; then
@@ -25,6 +37,16 @@ if [ "${1}" != "" ]; then
   get_rtc "${ip}"
   exit 0
 fi
+
+for i in "$@"
+do
+case $i in
+  -no_ping)
+    skip_ping=true
+    ;; 
+  -h)
+    print_usage
+done
 
 for ip in ${ips[@]}
 do
